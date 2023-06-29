64e820c186a4567a33d4d99585cd98dd4f91d658
3q3z4 <a2902769166@163.com>
2023-06-29 19:40:34 +0800

    第一次提交

--- _03_Convolution_Neural_Network/Convolution_Neural_Network.py
+++ _03_Convolution_Neural_Network/Convolution_Neural_Network.py
@@ -3,8 +3,6 @@
 
 import os
 
-os.system("sudo pip3 install torch")
-os.system("sudo pip3 install torchvision")
 
 import torch
 import torch.nn as nn
@@ -15,7 +13,84 @@ from torch.utils.data import DataLoader
     
 
 class NeuralNetwork(nn.Module):
-    pass
+    def __init__(self):
+        super(NeuralNetwork, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(64),
+            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(64),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(128),
+            nn.Conv2d(128, 128, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(128),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        self.classifier = nn.Sequential(
+            nn.Linear(128 * 8 * 8, 512),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm1d(512),
+            nn.Linear(512, 10)
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        return x
+
+def read_data():
+    transform_train = torchvision.transforms.Compose([
+        torchvision.transforms.RandomCrop(32, padding=4),
+        torchvision.transforms.RandomHorizontalFlip(),
+        torchvision.transforms.ToTensor(),
+        torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+
+    transform_val = torchvision.transforms.Compose([
+        torchvision.transforms.ToTensor(),
+        torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+
+    dataset_train = torchvision.datasets.CIFAR10(root='./data/exp03', train=True, download=True, transform=transform_train)
+    dataset_val = torchvision.datasets.CIFAR10(root='./data/exp03', train=False, download=True, transform=transform_val)
+
+    data_loader_train = DataLoader(dataset=dataset_train, batch_size=128, shuffle=True, num_workers=2)
+    data_loader_val = DataLoader(dataset=dataset_val, batch_size=128, shuffle=False, num_workers=2)
+
+    return dataset_train, dataset_val, data_loader_train, data_loader_val
+
+def train(model, data_loader_train, criterion, optimizer):
+    model.train()
+    for epoch in range(15):  # 增加训练轮数
+        running_loss = 0.0
+        for i, (inputs, labels) in enumerate(data_loader_train):
+            optimizer.zero_grad()
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+        print('Epoch [%d/%d], Loss: %.4f' % (epoch + 1, 20, running_loss / len(data_loader_train)))
+
+def evaluate(model, data_loader_val):
+    model.eval()
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for inputs, labels in data_loader_val:
+            outputs = model(inputs)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    accuracy = 100 * correct / total
+    print('Accuracy on validation set: %.2f %%' % accuracy)
 
 def read_data():
     # 这里可自行修改数据预处理，batch大小也可自行调整
